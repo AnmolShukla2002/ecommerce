529d5ce816eeea47d023328e737e831af0ce6dfc
Anmol Shukla <anmol.28422@gmail.com>
2024-01-15 02:20:31 +0530

    forgot password styling

--- src/app/(pages)/recover-password/page.tsx
+++ src/app/(pages)/recover-password/page.tsx
@@ -1,17 +1,37 @@
 import React from 'react'
 import { Metadata } from 'next'
-
-import { Gutter } from '../../_components/Gutter'
 import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import { RecoverPasswordForm } from './RecoverPasswordForm'
 
 import classes from './index.module.scss'
+import Link from 'next/link'
+import Image from 'next/image'
+import { RenderParams } from '../../_components/RenderParams'
 
 export default async function RecoverPassword() {
   return (
-    <Gutter className={classes.recoverPassword}>
-      <RecoverPasswordForm />
-    </Gutter>
+    <section className={classes.recoverPassword}>
+      <div className={classes.heroImg}>
+        <Link href="/">
+          <Image
+            src="/logo-white.svg"
+            alt="logo"
+            width={250}
+            height={23}
+            className={classes.logo}
+          />
+        </Link>
+      </div>
+      <div className={classes.formWrapper}>
+        <div className={classes.formContainer}>
+          <RenderParams className={classes.params} />
+          <div className={classes.formTitle}>
+            <h3>Forgot Password, eh?</h3>
+          </div>
+          <RecoverPasswordForm />
+        </div>
+      </div>
+    </section>
   )
 }
 
